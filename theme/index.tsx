b60082bb1b81a351d254ae67a6028ce01f04d454
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-02-05 21:15:30 -0300

    🎨 Add theme configuration with primary, secondary, and gray color palettes

---
+++ theme/index.tsx
@@ -0,0 +1,30 @@
+const theme = {
+  primary: {
+    main: "#004D61",
+    light: "#DEE9EA",
+    transparent: "rgba(0, 77, 97, 0.1)",
+    hover: "rgba(0, 77, 97, 0.7)",
+  },
+  secondary: {
+    main: "#47A138",
+    light: "#E4EDE3",
+    transparent: "rgba(71, 161, 56, 0.1)",
+    hover: "rgba(71, 161, 56, 0.7)",
+  },
+  gray: {
+    main: "#8B8B8B",
+    light: "#CCCCCC",
+    medium: "#CBCBCB",
+    dark: "#444444",
+    '100': "#F8F8F8",
+    '600': "#767676",
+  },
+  low: {
+    main: '#000000'
+  },
+  high: {
+    main: '#FFFFFF'
+  },
+};
+
+export default theme;
