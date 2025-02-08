b262f4b252c27198ca160ea71f80d8be2f983bed
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-02-08 12:24:17 -0300

    ✨ Add BottomTabBar component

---
+++ components/BottomTabBar.tsx
@@ -0,0 +1,77 @@
+import { useLinkBuilder } from '@react-navigation/native';
+import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Entypo from '@expo/vector-icons/Entypo';
+import styled from 'styled-components/native';
+import theme from '@/theme';
+
+const LABELS:{ [key: string ]:string } = {
+  Dashboard: 'Dashboard',
+  Transactions: 'Transações',
+  Account: 'Conta',
+}
+
+const ICONS:{ [key: string ]:(color:string) => React.ReactNode } = {
+  Dashboard: (color) => <MaterialIcons name="dashboard" size={24} color={color} />,
+  Transactions: (color) => <Entypo name="list" size={24} color={color} />,
+  Account: (color) => <MaterialIcons name="account-circle" size={24} color={color} />,
+}
+
+const BottomTabBar = ({ state, navigation }:BottomTabBarProps) => {
+  const { buildHref } = useLinkBuilder();
+
+  return (
+    <Bar>
+      {state.routes.map((route, index) => {
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        return (
+          <Item
+            key={route.key}
+            href={buildHref(route.name, route.params)}
+            onPress={onPress}
+          >
+            {ICONS[route.name](isFocused ? theme.secondary.main : theme.high.main)}
+            <Label isFocused={isFocused}>
+              {LABELS[route.name]}
+            </Label>
+          </Item>
+        );
+      })}
+    </Bar>
+  );
+};
+
+const Bar = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  height: 80px;
+  background-color: ${({ theme }) => theme.primary.main};
+`;
+
+const Item = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 16px;
+`;
+
+const Label = styled.Text`
+  color: ${({ theme, isFocused }) => isFocused ? theme.secondary.main : theme.high.main};
+  font-weight: 600;
+  font-size: 12px;
+`;
+
+export default BottomTabBar;
