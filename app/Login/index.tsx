2630de82353dbcea10221f887911763d3a4ea3de
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-02-08 11:56:40 -0300

    ✨ Update Login and Registration screens: integrate navigation and enhance button functionality

--- app/Login/index.tsx
+++ app/Login/index.tsx
@@ -1,35 +1,41 @@
 import { Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import Styled from './styled';
 import Input from '@/components/Input';
 import PasswordInput from '@/components/PasswordInput';
 import Button from '@/components/Button';
 
-const Login = () => (
-  <Styled.Scroll>
-    <Styled.ImageBox>
-      <Image
-        source={require('@/assets/images/login.png')}
-        style={{ width: 220, height: 200 }}
+const Login = () => {
+  const navigation = useNavigation();
+
+  return (
+    <Styled.Scroll>
+      <Styled.ImageBox>
+        <Image
+          source={require('@/assets/images/login.png')}
+          style={{ width: 220, height: 200 }}
+        />
+      </Styled.ImageBox>
+
+      <Styled.Title>
+        Login
+      </Styled.Title>
+
+      <Input
+        label="E-mail"
+        placeholder="Digite seu email"
       />
-    </Styled.ImageBox>
+      <PasswordInput />
 
-    <Styled.Title>
-      Login
-    </Styled.Title>
-
-    <Input
-      label="E-mail"
-      placeholder="Digite seu email"
-    />
-    <PasswordInput />
-
-    <Styled.ButtonBox>
-      <Button
-        title="Entrar"
-        color="secondary"
-      />
-    </Styled.ButtonBox>
-  </Styled.Scroll>
-);
+      <Styled.ButtonBox>
+        <Button
+          title="Entrar"
+          color="secondary"
+          onPress={() => navigation.navigate('Tabs')}
+        />
+      </Styled.ButtonBox>
+    </Styled.Scroll>
+  );
+};
 
 export default Login;
--- app/Registration/index.tsx
+++ app/Registration/index.tsx
@@ -1,4 +1,5 @@
 import { Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import Styled from './styled';
 import Input from '@/components/Input';
 import PasswordInput from '@/components/PasswordInput';
@@ -6,44 +7,49 @@ import Checkbox from 'expo-checkbox';
 import theme from '@/theme';
 import Button from '@/components/Button';
 
-const Registration = () => (
-  <Styled.Scroll>
-    <Styled.ImageBox>
-      <Image
-        source={require('@/assets/images/registration.png')}
-        style={{ width: 220, height: 162 }}
+const Registration = () => {
+  const navigation = useNavigation();
+
+  return (
+    <Styled.Scroll>
+      <Styled.ImageBox>
+        <Image
+          source={require('@/assets/images/registration.png')}
+          style={{ width: 220, height: 162 }}
+        />
+      </Styled.ImageBox>
+
+      <Styled.Title>
+        Preencha os campos abaixo para criar sua conta corrente!
+      </Styled.Title>
+
+      <Input
+        label="Nome"
+        placeholder="Digite seu nome completo"
       />
-    </Styled.ImageBox>
-
-    <Styled.Title>
-      Preencha os campos abaixo para criar sua conta corrente!
-    </Styled.Title>
-
-    <Input
-      label="Nome"
-      placeholder="Digite seu nome completo"
-    />
-    <Input
-      label="E-mail"
-      placeholder="Digite seu email"
-    />
-    <PasswordInput />
-
-    <Styled.CheckBoxContainer>
-      <Checkbox color={theme.secondary.main} />
-      <Styled.CheckboxLabel>
-        Li e estou ciente quanto às condições de tratamento dos meus dados
-        conforme descrito na Política de Privacidade do banco.
-      </Styled.CheckboxLabel>
-    </Styled.CheckBoxContainer>
-
-    <Styled.ButtonBox>
-      <Button
-        title="Criar conta"
-        color="secondary"
+      <Input
+        label="E-mail"
+        placeholder="Digite seu email"
       />
-    </Styled.ButtonBox>
-  </Styled.Scroll>
-);
+      <PasswordInput />
+
+      <Styled.CheckBoxContainer>
+        <Checkbox color={theme.secondary.main} />
+        <Styled.CheckboxLabel>
+          Li e estou ciente quanto às condições de tratamento dos meus dados
+          conforme descrito na Política de Privacidade do banco.
+        </Styled.CheckboxLabel>
+      </Styled.CheckBoxContainer>
+
+      <Styled.ButtonBox>
+        <Button
+          title="Criar conta"
+          color="secondary"
+          onPress={() => navigation.navigate('Tabs')}
+        />
+      </Styled.ButtonBox>
+    </Styled.Scroll>
+  );
+};
 
 export default Registration;
