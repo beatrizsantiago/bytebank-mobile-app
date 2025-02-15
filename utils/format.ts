f8ac8504a77ebd2d67d5488618631624d37a9bfc
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-02-15 18:04:53 -0300

    ✨ Add currency formatting utilities: implement formatCurrency and currencyToFloat functions

--- utils/format.ts
+++ utils/format.ts
@@ -5,3 +5,22 @@ export const money = (value?:number | null):string => (value
 export const filename = (name:string) => (name.length > 16
     ? `${name.substring(0, 16)}...${name.substring(name.length - 4)}`
     : name);
+
+export const formatCurrency = (input:string) => {
+  let numericValue = input.replace(/\D/g, "");
+
+  let formattedValue = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(parseFloat(numericValue) / 100);
+
+  return formattedValue;
+};
+
+export const currencyToFloat = (value:string) => {
+  if (!value) return 0;
+  
+  const numericValue = value.replace(/[R$\s.]/g, '').replace(',', '.');
+  
+  return parseFloat(numericValue);
+};
