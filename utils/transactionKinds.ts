3b9c273627f7fdaf7017b30be86f6fe88ea0e224
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-02-16 20:31:05 -0300

    ✨ Update transactionKinds: add new investment and retirement types to transaction labels

--- utils/transactionKinds.ts
+++ utils/transactionKinds.ts
@@ -2,7 +2,10 @@ export const KIND_LABEL:{ [key: string]: string } = {
   DEPOSIT: 'Depósito',
   DOC_TED: 'Transferência',
   CURRENCY_EXCHANGE: 'Câmbio de Moeda',
-  LEASING: 'Empréstimo e Financiamento',
+  INVESTMENT_FOUND: 'Fundo de Investimento',
+  PUBLIC_CONTRACTS: 'Tesouro Direto',
+  PRIVATE_RETIREMENT: 'Previdência Privada',
+  STOCK_EXCHANGE: 'Bolsa de Valores',
 };
 
 export const KIND_LIST = Object.keys(KIND_LABEL).map((key) => ({ label: KIND_LABEL[key], value: key }));
