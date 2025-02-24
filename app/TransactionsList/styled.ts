6e481c182b5625c541949fbfd60c44e686fb926f
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-02-24 17:02:26 -0300

    ✨ Update TransactionsList styles: replace ScrollView with FlatList for improved performance and remove unused search components

--- app/TransactionsList/styled.ts
+++ app/TransactionsList/styled.ts
@@ -1,11 +1,10 @@
 import styled from 'styled-components/native';
-import Feather from '@expo/vector-icons/Feather';
 
 const Header = styled.View`
   padding: 16px 16px 0px 16px;
 `;
 
-const Scroll = styled.ScrollView`
+const List = styled.FlatList`
   padding: 0px 16px;
 `;
 
@@ -30,35 +29,11 @@ const ExtractLabel = styled.Text`
   font-weight: 700;
 `;
 
-const SearchRow = styled.View`
-  flex-direction: row;
-  align-items: center;
-  margin-bottom: 16px;
-`;
-
-const SearchInput = styled.TextInput`
-  background-color: ${({ theme }) => theme.high.main};
-  padding: 16px;
-  padding-right: 48px;
-  border-radius: 8px;
-  width: 100%;
-  height: 50px;
-`;
-
-const SearchIcon = styled(Feather)`
-  position: absolute;
-  right: 16px;
-  color: ${({ theme }) => theme.primary.main};
-`;
-
 export default {
   Header,
-  Scroll,
+  List,
   TopRow,
   IconsRow,
   IconButton,
   ExtractLabel,
-  SearchInput,
-  SearchRow,
-  SearchIcon,
 };
