470632dfeeb87c3692b59851ff360cd444cced39
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-02-28 12:01:02 -0300

    ✨ Add formatDate utility function for date formatting in Brazilian Portuguese

--- utils/format.ts
+++ utils/format.ts
@@ -28,3 +28,7 @@ export const currencyToFloat = (value:string) => {
 export const floatToCurrency = (value:number) => {
   return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value);
 };
+
+export const formatDate = (date:Date) => {
+  return new Intl.DateTimeFormat('pt-BR').format(date);
+};
